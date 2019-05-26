bfa8359cbb6748ad0d05e618fdf34ad3f499a8b5
faahta <fasso143@gmail.com>
2019-05-26 11:53:51 +0200

    first commit

---
+++ prime_search.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+
+int t,n;
+
+typedef struct prime_search{
+	int num;
+	pthread_mutex_t lock;
+	pthread_cond_t cond;
+	int count;
+	int is_over;
+	
+}prime_search_t;
+
+prime_search_t p_search; 
+
+int 
+is_prime(int n){
+	int c;
+	if(n < 2) return 0;
+	for(c = 2; c < n/2; c++){
+		if((n%c) == 0)
+			return 0;
+	}
+	return 1;
+}
+static void *
+search_threads(void *arg){
+
+	int *id = (int *)arg;
+	
+	while(1){
+		sleep(1);
+		pthread_mutex_lock(&p_search.lock);
+		
+		p_search.num++;
+		if(is_prime(p_search.num)){
+			printf("%d\n",p_search.num);
+			printf("thread %d searching prime: num: %d \n", *id, p_search.num);
+			p_search.count++;
+		}
+		
+		while(p_search.count >= n){
+			pthread_cond_wait(&p_search.cond, &p_search.lock);
+			printf("thread %d exiting\n",*id);
+			p_search.is_over = 1;
+			break;
+		}
+		if(p_search.is_over)
+			break;
+		pthread_mutex_unlock(&p_search.lock);
+	}
+	pthread_mutex_unlock(&p_search.lock);
+return NULL;
+
+}
+void 
+init(){
+	p_search.num = 1;
+	p_search.count = 0;
+	p_search.is_over = 0;
+	pthread_mutex_init(&p_search.lock, NULL);
+	pthread_cond_init(&p_search.cond, NULL);
+}
+
+int 
+main(int argc, char *argv[]){
+
+	t = atoi(argv[1]);
+	n = atoi(argv[2]);
+	init();
+	
+	pthread_t *th_sp;
+	th_sp = (pthread_t *)malloc(t * sizeof(pthread_t));
+	int i;
+	int *pi;
+	for(i=0;i<t;i++){
+		pi = (int *)malloc(sizeof(int));
+		*pi = i;
+		sleep(1);
+		pthread_create(&th_sp[i],NULL,search_threads, (void *)pi);
+	}
+	sleep(15);
+	
+	pthread_mutex_lock(&p_search.lock);
+	/*wake up all waiting threads*/
+	pthread_cond_broadcast(&p_search.cond);
+	pthread_mutex_unlock(&p_search.lock);
+	
+	for(i=0;i<t;i++){
+		pthread_join(th_sp[i],NULL);
+	}
+
+	return 0;
+}
